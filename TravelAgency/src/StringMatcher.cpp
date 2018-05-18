e184865dff56da6d539c64a4f33f4c8a2988696a
Duarte-Frazao <duartemarquesmano@gmail.com>
2018-05-18 01:21:44 +0100

    Removed extra files

--- TravelAgency/src/StringMatcher.cpp
+++ TravelAgency/src/StringMatcher.cpp
@@ -1,7 +1,73 @@
 #include "StringMatcher.h"
-#include "StringUtilities.h"
+
+using namespace std;
+
+/*
+ * Based on the fact that the characters we wanted to extract are represented by two bytes and the first one is negative we:
+ *	-Convert the invalid chars to their code
+ *	-Iterate through the string, if we find a negative char it means it's one of the strange characters
+ *	-We discard that char (first byte)
+ *	-Then substitute the second byte by the desired char replacer
+ */
+string preProcessingChars(string text){
+
+	cout << "String before pre processing: "<< text << endl;
+
+	//Get ascii code from the invalid characters
+	string invalidChars= "ãâáàÁÀÃÂéêÉÊíÍóòôõÓÒÔÕũûúŨÛÚçÇ";
+	string valid=  		 "aaaaaaaaeeeeiioooooooouuuuuucc";
+	vector<char> invalid;
+	for(int i = 1; i < invalidChars.length();i+=2)invalid.push_back((int) invalidChars[i]);
+
+	//Transform valid letters to lower case
+	transform(text.begin(), text.end(), text.begin(), ::tolower);
 
 
+	//Remove invalid chars
+	for(auto it=text.begin(); it !=text.end();it++){
+		for(uint j = 0; j < valid.length();j++){
+			if((int) (*it)<0 && (int) (*(it +1))==invalid.at(j)){
+
+				it=text.erase(it);
+				(*it)=valid[j];
+				break;
+			}
+		}
+	}
+	cout << "String after  pre processing: "<< text << endl;
+	cout << "---------"<< endl;
+
+	return text;
+}
+
+bool isCommmonConjunction(string text){
+	vector<string> conjs{"de","da","e", "a","do","o"};
+
+
+	for(int i = 0; i < conjs.size();i++){
+		if(conjs.at(i).length() ==text.length() &&kmpMatcher(text,conjs.at(i))>0) return true;
+	}
+	return false;
+}
+
+vector<string> preProcessString(string text){
+
+	for(int i =0; i < text.length();i++){
+		if(text[i]=='-')text[i]=' ';
+	}
+
+	vector<string> result;
+	char trash;
+	stringstream s(text);
+	while(!s.eof()){
+		string temp;
+		s>>temp;
+		if(!isCommmonConjunction(temp))result.push_back(temp);
+	}
+
+
+	return result;
+}
 
 std::vector<unsigned int> naiveMatcher(std::string text, std::string pattern){
 	std::vector<unsigned int> occurrences;
--- TravelAgency/src/StringMatcher.h
+++ TravelAgency/src/StringMatcher.h
@@ -1,3 +1,14 @@
+
+
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
 void preKmpMatcher(std::string pattern, int pi[]);
 int kmpMatcher(std::string text, std::string pattern);
 int editDistance(std::string pattern, std::string text);
+std::string preProcessingChars(std::string text);
+std::vector<std::string> preProcessString(std::string text);
+bool isCommmonConjunction(std::string text);
+
